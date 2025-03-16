1489b57ae655d50f16dd2da192d2d77a592d8f51
hliebovkyrylo <kirilglebov06@gmail.com>
2025-03-16 16:24:58 +0200

    fix: changed padding

--- apps/web/src/modules/common/Layout/MainLayout.tsx
+++ apps/web/src/modules/common/Layout/MainLayout.tsx
@@ -5,7 +5,7 @@ export const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="w-full flex flex-col gap-6 min-h-screen px-[120px]">
       <Header />
-      <div>{children}</div>
+      <div className="pb-10">{children}</div>
     </main>
   );
 };
